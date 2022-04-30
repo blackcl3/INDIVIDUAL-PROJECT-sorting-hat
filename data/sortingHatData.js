62413faca1530408937b416f41f2e9e724331a2c
Christian Black <blackl3@gmail.com>
2022-04-30 11:26:57 -0500

    Added starting data to sortingHatData.js file

---
+++ data/sortingHatData.js
@@ -0,0 +1,23 @@
+export const sortingHataData = [
+  {
+    id: 1, 
+    name: "Harry Potter", 
+    house: "Gryffindor", 
+    isExpelled: false, 
+    imageURL: ""
+  },
+  {
+    id: 2, 
+    name: "Dumbledore", 
+    house: "Gryffindor", 
+    isExpelled: false, 
+    imageURL: ""
+  },
+  {
+    id: 3, 
+    name: "Tom Riddle", 
+    house: "Slytherin", 
+    isExpelled: false, 
+    imageURL: ""
+  },
+]; 
