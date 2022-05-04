33ecd501f90ccb6fb0ae2311bdba0b1f9f3d5ac3
Christian Black <blackl3@gmail.com>
2022-05-03 20:02:28 -0500

    Continuing to work on making cards/buttons visible with functions

--- components/formRender.js
+++ components/formRender.js
@@ -3,12 +3,13 @@ import { renderToDom } from "../utils/renderToDom.js";
 const formRender = () => {
   let domString = 
   `<form id="nameInputForm">
-    <div class="form-floating">
-      <label for="nameInputTextArea">Enter Your Name Here</label>
-      <textarea class="form-control" placeholder="Enter Your Name Here" id="nameInputTextArea" required></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>`
+        <div class="form-floating">
+          <label for="nameInputTextArea">Enter Your Name Here</label>
+          <textarea class="form-control" placeholder="Enter Your Name Here" id="nameInputTextArea" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  `
   renderToDom("#name-form-container", domString)
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,20 @@ import cardRender from "../components/cardRender.js";
 import buttonRow from "../components/buttonRender.js";
 import formRender from "../components/formRender.js";
 import eventListeners from "./eventListeners.js";
+import { visibilityFunction } from "./visibilityFunction.js";
+
+const openPage = () => {
+  formRender()
+}
 
 const startApp = () => {
   formRender()
   buttonRow()
   cardRender(sortingHataData, "#card-container")
+  visibilityFunction("#card-container")
+  visibilityFunction("#filter-btn-container")
   eventListeners()
 }
 
-window.onload = function() {
-  startApp()
-}
+
+startApp()
